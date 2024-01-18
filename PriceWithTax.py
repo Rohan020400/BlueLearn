44e47bb3eb3c5021cfd40ec31449de443cd7e6ec
Rohan Alam <eng20cs0293.rohanalam@gmail.com>
2024-01-19 04:23:46 +0530

    Update PriceWithTax.py

--- PriceWithTax.py
+++ PriceWithTax.py
@@ -1,3 +1,5 @@
+# Design a program that takes the prices of items a user bought 
+# and calculates the total cost, including tax
 PriceTotal= 0
 prices=(input("Enter the prices:").split())
 for i in prices:
